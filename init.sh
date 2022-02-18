4dde4235ec33fafb9b9b1c30cbfc2a1019dbf635
tien.luckyfor <tien.luckyfor@gmail.com>
2022-02-18 09:34:23 +0700

    init sh

--- init.sh
+++ init.sh
@@ -6,3 +6,7 @@ git clone https://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@github.com/tienluckyf
 cd bash-script; 
 source .env;
 source scripts/init_lemp.sh;
+
+
+echo 'P_PATH="/var/www/bash-script"; 
+alias ppe="source $P_PATH/.env; bash $P_PATH/menu.sh"' > ~/.bash_aliases; source ~/.bash_aliases;
