0474d83ed715a9b3eb7b0780eac95f8685af1a01
tien.luckyfor <tien.luckyfor@gmail.com>
2022-02-18 09:27:57 +0700

    init sh

--- init.sh
+++ init.sh
@@ -3,7 +3,5 @@ mkdir -p /var/www;
 cd /var/www; 
 git clone https://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@github.com/tienluckyfor/bash-script.git; 
 cd bash-script; 
-git config --global credential.helper store; 
-git pull; 
 source .env;
 bash scripts/init_lemp.sh;
